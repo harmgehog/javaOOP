6428969e61ecc3b85395fa6b01c3e8f076d80404
harmgehog <polstia@gmail.com>
2023-06-10 18:42:01 +0300

    HomeWork4 штвуindexOutOfBounds

--- src/HomeWork4/Fruit.java
+++ src/HomeWork4/Fruit.java
@@ -2,14 +2,14 @@ package HomeWork4;
 
 public abstract class Fruit {
 
-    private String name;
+    private final String name;
     private final float weight;
 
     public float getWeight() { return weight; }
 
     public String getName() { return name; }
 
-    public Fruit(float weight, String Name) {
+    public Fruit(float weight, String name) {
         this.weight = weight;
         this.name = name;
     }
